2e08fdb9c4df09833393f90eea63c5d3915b32cf
Denis Turcu <denis.turcu.11@gmail.com>
2024-05-16 15:19:08 -0400

    cleanup and format

--- helper_functions/assertion_tests.py
+++ helper_functions/assertion_tests.py
@@ -4,25 +4,53 @@ from conversions import rho24sig, convert2mainSI
 from prefixes import remove_prefix, add_prefix
 
 
-def run_tests(N=10,err=1e-12):
-    for i in np.random.randn(N):
-        assert np.abs(remove_prefix(i,'d')-i/10)<err, f'"remove_prefix" function fails on "d" example for value {i}'
-        assert np.abs(   add_prefix(i,'d')-i*10)<err, f'"add_prefix" function fails on "d" example for value {i}'
+def run_tests(N: int = 10, err: float = 1e-12) -> str:
+    """Run tests for the helper functions in the "conversions" and "prefixes" modules.
 
-        assert np.abs(add_prefix(i,'c')-i*100)<err, f'"add_prefix" function fails on "c" example for value {i}'
+    Args:
+        N (int, optional): Number of test trials. Defaults to 10.
+        err (float, optional): Error threshold for deciding what counts as "0" error. Defaults to 1e-12.
 
-        if np.abs(i)>err:
-            assert np.abs(rho24sig(i)-1/i)<err, f'"rho24sig" fails on {i}'
-            assert np.abs(rho24sig(np.array(i))-1/i)<err, f'"rho24sig" fails on "np.array({i})"'
-            assert np.abs(rho24sig(np.array([i]))-1/i)<err, f'"rho24sig" fails on "np.array([{i}])"'
+    Returns:
+        str: "Success!" if all tests pass. Assertion error otherwise.
+    """
+    for test_val in np.random.randn(N):
+        assert (
+            np.abs(remove_prefix(test_val, "d") - test_val / 10) < err
+        ), f'"remove_prefix" function fails on "d" example for value {test_val}'
+        assert (
+            np.abs(add_prefix(test_val, "d") - test_val * 10) < err
+        ), f'"add_prefix" function fails on "d" example for value {test_val}'
 
-        assert np.abs(convert2mainSI(i)-i)<err,                     f'"convert2mainSI" fails on {i}'
-        assert np.abs(convert2mainSI((i,'d'))-i/10)<err,            f'"convert2mainSI" fails on ({i},"d")'
-        assert np.abs(convert2mainSI((i,'c'))-i/100)<err,           f'"convert2mainSI" fails on ({i},"c")'
-        assert np.abs(convert2mainSI((i,'u','c.-1'))-i/10000)<err,   f'"convert2mainSI" fails on ({i},("u","c.-"))'
-        assert np.abs(convert2mainSI((i,'k','c.2'))-i/10)<err,      f'"convert2mainSI" fails on ({i},("k","c.2"))'
+        assert (
+            np.abs(add_prefix(test_val, "c") - test_val * 100) < err
+        ), f'"add_prefix" function fails on "c" example for value {test_val}'
 
-    assert np.abs(remove_prefix(90,'deg')-np.pi/2) < err, '"remove_prefix" function fails on degree <-> rad conversion.'
-    assert np.abs(add_prefix(np.pi/4,'deg')-45) < err,    '"add_prefix" function fails on degree <-> rad conversion.'
-    return 'Success!'
+        if np.abs(test_val) > err:
+            assert np.abs(rho24sig(test_val) - 1 / test_val) < err, f'"rho24sig" fails on {test_val}'
+            assert (
+                np.abs(rho24sig(np.array(test_val)) - 1 / test_val) < err
+            ), f'"rho24sig" fails on "np.array({test_val})"'
+            assert (
+                np.abs(rho24sig(np.array([test_val])) - 1 / test_val) < err
+            ), f'"rho24sig" fails on "np.array([{test_val}])"'
 
+        assert np.abs(convert2mainSI(test_val) - test_val) < err, f'"convert2mainSI" fails on {test_val}'
+        assert (
+            np.abs(convert2mainSI((test_val, "d")) - test_val / 10) < err
+        ), f'"convert2mainSI" fails on ({test_val},"d")'
+        assert (
+            np.abs(convert2mainSI((test_val, "c")) - test_val / 100) < err
+        ), f'"convert2mainSI" fails on ({test_val},"c")'
+        assert (
+            np.abs(convert2mainSI((test_val, "u", "c.-1")) - test_val / 10000) < err
+        ), f'"convert2mainSI" fails on ({test_val},("u","c.-"))'
+        assert (
+            np.abs(convert2mainSI((test_val, "k", "c.2")) - test_val / 10) < err
+        ), f'"convert2mainSI" fails on ({test_val},("k","c.2"))'
+
+    assert (
+        np.abs(remove_prefix(90, "deg") - np.pi / 2) < err
+    ), '"remove_prefix" function fails on degree <-> rad conversion.'
+    assert np.abs(add_prefix(np.pi / 4, "deg") - 45) < err, '"add_prefix" function fails on degree <-> rad conversion.'
+    return "Success!"
